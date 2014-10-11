59e3b0a05481b241c81789ac687ff6b7d153d511
Kyle McKee <mckee.kyle@gmail.com>
2014-10-11 10:06:29 -0400

    Static routing added.

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-var express = require('express');
+var express = require('express'),
+    stylus = require('stylus'),
+    logger = require('morgan');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -6,11 +8,20 @@ var app = express();
 
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+app.use(stylus.middleware(
+      {
+        src: __dirname + '/public',
+        compile: function(str, path) {
+          return stylus(str).set('filename', path);
+        }
+      }));
+app.use(logger('dev'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req, res) {
   res.render('index');
 });
 
-var port = 3030;
+var port = 8084;
 app.listen(port);
 console.log('listening on port ' + port + '...');
